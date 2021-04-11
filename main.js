f2f7769f7080ca14c80eb1431d17d0d5aa5bab25
Gabriel Casanova <con.casanovaproductions@gmail.com>
2021-04-11 00:51:32 -0300

    Modified bree assets and made level 3 a bit easier

--- main.js
+++ main.js
@@ -73,11 +73,11 @@ var levels = [
 			"assets/backgrounds/breeSkies.png",
 			"assets/backgrounds/breeBuildings0.png",
 			"assets/backgrounds/breeBuildings1.png"
-		]
+		],
 		music: "assets/music/hobbiton-1.mp3"
 	}
 ]
 
-var currentLevel = 3
+var currentLevel = 2
 
 var game = new Phaser.Game(config)
